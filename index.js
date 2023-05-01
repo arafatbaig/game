7e35619b032cd744e138ff5722c320c1a94e90d7
Mirza Arafat Baig <103023081+arafatbaig@users.noreply.github.com>
2023-05-01 14:58:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const playerText =document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtn = document.querySelectorAll(".choiceBtn");
+
+let player ;
+let computer;
+let result;
+
+choiceBtn.forEach(button=> button.addEventListener("click",()=>{
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent =`Player:${player}`;
+    computerText.textContent =`Computer:${computer}`;
+    resultText.textContent = checkWinner();
+
+
+}));
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random()*3)+1;
+    switch(randNum){
+    case 1:
+        computer ="Rock";
+        break;
+    case 2:
+        computer = "PAPER";
+        break;
+    case 3 :
+        computer = "SCISSORS";
+        break;
+    }
+}
+
+function checkWinner(){
+    if(player == computer){
+        return "Draw!";
+    }
+    else if(computer =="ROCK"){
+        return(player =="PAPER")?"YPU WIN!":"YOU LOSE!"
+    }
+    else if (computer =="PAPER"){
+        return(player =="SCISSOR")?"YOU WIN!":"YOU LOSE!"
+    }
+    else if(computer =="SCISSORS"){
+        return(player =="ROCK")?"YOU WIN!":"YOU LOSE!"
+    }
+}
